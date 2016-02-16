f1ebdd449fa56f85d590b05b50cbbf209a5de767
Jaime Melis <jmelis@opennebula.org>
2016-02-16 14:15:26 +0100

    Remove the dependency of boot2docker parameters
    * The user specifies what image to use
    * A special startup script to adapt image to docker

--- opennebula.go
+++ opennebula.go
@@ -1,10 +1,10 @@
 package opennebula
 
 import (
+	"encoding/base64"
 	"errors"
 	"fmt"
 	"io/ioutil"
-	"strconv"
 	"time"
 
 	"github.com/OpenNebula/goca"
@@ -17,26 +17,49 @@ import (
 
 type Driver struct {
 	*drivers.BaseDriver
-	NetworkName     string
-	NetworkOwner    string
-	NetworkId       string
-	CPU             string
-	VCPU            string
-	Memory          string
-	DiskSize        string
-	Boot2DockerURL  string
-	DatastoreId     string
+	NetworkName    string
+	NetworkOwner   string
+	NetworkId      string
+	ImageName      string
+	ImageOwner     string
+	ImageId        string
+	CPU            string
+	VCPU           string
+	Memory         string
+	DiskSize       string
+	ImageDevPrefix string
 }
 
 const (
-	defaultTimeout        = 1 * time.Second
-	defaultSSHUser        = "docker"
-	defaultCPU            = "1"
-	defaultVCPU           = ""
-	defaultMemory         = "1024"
-	defaultDiskSize       = "20000"
-	defaultBoot2DockerURL = "https://s3.eu-central-1.amazonaws.com/one-boot2d/boot2docker-v1.9.1.iso"
-	defaultDatastoreId    = "1"
+	defaultTimeout  = 1 * time.Second
+	defaultSSHUser  = "docker"
+	defaultCPU      = "1"
+	defaultVCPU     = "1"
+	defaultMemory   = "1024"
+	defaultDiskSize = "20000"
+
+	contextScript = `#!/bin/bash
+
+USERNAME=docker
+GROUPNAME=docker
+USER_HOME=/var/lib/docker
+
+groupadd $GROUPNAME
+useradd -m -d $USER_HOME -g $USERNAME $GROUPNAME
+
+AUTH_DIR="${USER_HOME}/.ssh"
+AUTH_FILE="${AUTH_DIR}/authorized_keys"
+
+mkdir -m0700 -p $AUTH_DIR
+
+echo "$DOCKER_SSH_PUBLIC_KEY" >> $AUTH_FILE
+
+chown "${USERNAME}": ${AUTH_DIR} ${AUTH_FILE}
+chmod 600 $AUTH_FILE
+
+echo 'Defaults:$USERNAME !requiretty' >> /etc/sudoers.d/$USERNAME
+echo "$USERNAME ALL=(ALL:ALL) NOPASSWD: ALL" >> /etc/sudoers.d/$USERNAME
+`
 )
 
 func NewDriver(hostName, storePath string) *Driver {
@@ -75,7 +98,7 @@ func (d *Driver) GetCreateFlags() []mcnflag.Flag {
 			Name:   "opennebula-vcpu",
 			Usage:  "VCPUs for the VM",
 			EnvVar: "ONE_VCPU",
-			Value:  defaultCPU,
+			Value:  defaultVCPU,
 		},
 		mcnflag.StringFlag{
 			Name:   "opennebula-disk-size",
@@ -102,16 +125,28 @@ func (d *Driver) GetCreateFlags() []mcnflag.Flag {
 			Value:  "",
 		},
 		mcnflag.StringFlag{
-			Name:   "opennebula-datastore-id",
-			Usage:  "Datastore ID of the Boot2Docker image",
-			EnvVar: "ONE_DATASTORE_ID",
-			Value:  defaultDatastoreId,
+			Name:   "opennebula-image-name",
+			Usage:  "Image to use as the OS",
+			EnvVar: "ONE_IMAGE_NAME",
+			Value:  "",
 		},
 		mcnflag.StringFlag{
-			Name:   "opennebula-boot2docker-url",
-			Usage:  "The URL of the boot2docker image. By default it uses one hosted by OpenNebula.org",
-			EnvVar: "ONE_BOOT2DOCKER_URL",
-			Value:  defaultBoot2DockerURL,
+			Name:   "opennebula-image-id",
+			Usage:  "Image ID to use as the OS",
+			EnvVar: "ONE_IMAGE_ID",
+			Value:  "",
+		},
+		mcnflag.StringFlag{
+			Name:   "opennebula-image-owner",
+			Usage:  "Owner of the image to use as the OS",
+			EnvVar: "ONE_IMAGE_OWNER",
+			Value:  "",
+		},
+		mcnflag.StringFlag{
+			Name:   "opennebula-dev-prefix",
+			Usage:  "Dev prefix to use for the images: 'vd', 'sd', 'hd', etc...",
+			EnvVar: "ONE_IMAGE_DEV_PREFIX",
+			Value:  "",
 		},
 	}
 }
@@ -124,17 +159,28 @@ func (d *Driver) SetConfigFromFlags(flags drivers.DriverOptions) error {
 	d.NetworkName = flags.String("opennebula-network-name")
 	d.NetworkId = flags.String("opennebula-network-id")
 	d.NetworkOwner = flags.String("opennebula-network-owner")
-	d.DatastoreId = flags.String("opennebula-datastore-id")
-	d.Boot2DockerURL = flags.String("opennebula-boot2docker-url")
+	d.ImageId = flags.String("opennebula-image-id")
+	d.ImageName = flags.String("opennebula-image-name")
+	d.ImageOwner = flags.String("opennebula-image-owner")
 	d.SSHUser = flags.String("opennebula-ssh-user")
+	d.ImageDevPrefix = flags.String("opennebula-dev-prefix")
 
 	if d.NetworkName == "" && d.NetworkId == "" {
 		return errors.New("Please specify a network to connect to with --opennebula-network-name or --opennebula-network-id.")
 	}
 
- 	if d.NetworkName != "" && d.NetworkId != "" {
-		return errors.New("Please specify a network to connect to either with  --opennebula-network-name or --opennebula-network-id, not both.")
+	if d.NetworkName != "" && d.NetworkId != "" {
+		return errors.New("Please specify only one of: --opennebula-network-name or --opennebula-network-id, not both.")
 	}
+
+	if d.ImageName == "" && d.ImageId == "" {
+		return errors.New("Please specify a image to use as the OS with --opennebula-image-name or --opennebula-image-id.")
+	}
+
+	if d.ImageName != "" && d.ImageId != "" {
+		return errors.New("Please specify only one of: --opennebula-image-name or --opennebula-image-id, not both.")
+	}
+
 	return nil
 }
 
@@ -155,60 +201,7 @@ func (d *Driver) PreCreateCheck() error {
 }
 
 func (d *Driver) Create() error {
-	var (
-		err       error
-		b2d_id    uint
-		ds_id     uint64
-		b2d_image *goca.Image
-	)
-
-	// Import Boot2Docker
-	b2d_name := fmt.Sprintf("b2d-%s", d.MachineName)
-
-	b2d_image, err = goca.NewImageFromName(b2d_name)
-	if err != nil {
-		b2d_template := goca.NewTemplateBuilder()
-		b2d_template.AddValue("name", b2d_name)
-		b2d_template.AddValue("path", d.Boot2DockerURL)
-
-		ds_id, err = strconv.ParseUint(d.DatastoreId, 10, 32)
-		if err != nil {
-			return err
-		}
-
-		b2d_id, err = goca.CreateImage(b2d_template.String(), uint(ds_id))
-		if err != nil {
-			return err
-		}
-
-		b2d_image = goca.NewImage(b2d_id)
-
-		b2d_state := ""
-		for b2d_state != "READY" {
-			err = b2d_image.Info()
-			if err != nil {
-				return err
-			}
-
-			b2d_state, err = b2d_image.StateString()
-			if err != nil {
-				return err
-			}
-
-			switch b2d_state {
-			case "INIT", "LOCKED":
-				time.Sleep(1 * time.Second)
-			case "READY":
-			default:
-				log.Errorf("Unexpected image state %s", b2d_state)
-				return errors.New("Unexpected image state")
-			}
-		}
-
-		log.Infof("Boot2Docker image registered...")
-	} else {
-		b2d_id = b2d_image.Id
-	}
+	var err error
 
 	log.Infof("Creating SSH key...")
 	if err := ssh.GenerateSSHKey(d.GetSSHKeyPath()); err != nil {
@@ -222,6 +215,7 @@ func (d *Driver) Create() error {
 
 	// Create template
 	template := goca.NewTemplateBuilder()
+
 	template.AddValue("NAME", d.MachineName)
 	template.AddValue("CPU", d.CPU)
 	template.AddValue("MEMORY", d.Memory)
@@ -233,26 +227,38 @@ func (d *Driver) Create() error {
 	vector := template.NewVector("NIC")
 	if d.NetworkName != "" {
 		vector.AddValue("NETWORK", d.NetworkName)
-  	 	if d.NetworkOwner != "" {
+		if d.NetworkOwner != "" {
 			vector.AddValue("NETWORK_UNAME", d.NetworkOwner)
 		}
 	}
-        if d.NetworkId != "" {
+
+	if d.NetworkId != "" {
 		vector.AddValue("NETWORK_ID", d.NetworkId)
 	}
-	vector = template.NewVector("DISK")
-	vector.AddValue("IMAGE_ID", b2d_id)
-	vector.AddValue("DEV_PREFIX", "sd")
 
 	vector = template.NewVector("DISK")
-	vector.AddValue("FORMAT", "raw")
-	vector.AddValue("TYPE", "fs")
+
+	if d.ImageId != "" {
+		vector.AddValue("IMAGE_ID", d.ImageId)
+	} else {
+		vector.AddValue("IMAGE", d.ImageName)
+		if d.ImageOwner != "" {
+			vector.AddValue("IMAGE_UNAME", d.ImageOwner)
+		}
+	}
+
 	vector.AddValue("SIZE", string(d.DiskSize))
-	vector.AddValue("DEV_PREFIX", "sd")
+
+	if d.ImageDevPrefix != "" {
+		vector.AddValue("DEV_PREFIX", d.ImageDevPrefix)
+	}
 
 	vector = template.NewVector("CONTEXT")
 	vector.AddValue("NETWORK", "YES")
-	vector.AddValue("SSH_PUBLIC_KEY", string(pubKey))
+	vector.AddValue("SSH_PUBLIC_KEY", "$USER[SSH_PUBLIC_KEY]")
+	vector.AddValue("DOCKER_SSH_PUBLIC_KEY", string(pubKey))
+	contextScript64 := base64.StdEncoding.EncodeToString([]byte(contextScript))
+	vector.AddValue("START_SCRIPT_BASE64", contextScript64)
 
 	vector = template.NewVector("GRAPHICS")
 	vector.AddValue("LISTEN", "0.0.0.0")
